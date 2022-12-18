54df910264e6317398583062182cf122c8a1ded8
Hideki Natsumi <96385473+HidekiNatsumi@users.noreply.github.com>
2022-12-18 01:41:14 +0100

    changes

--- login/validation.js
+++ login/validation.js
@@ -1,12 +1,14 @@
 let Username = "admin";
 let Password = "admin";
-
+let loggedin = false;
 document.querySelector(".submit").addEventListener("click", function () {
   let username = document.querySelector(".username").value;
   let password = document.querySelector(".password").value;
   if (username === Username && password === Password) {
     alert("Login Successful");
+    loggedin = true;
   } else {
     alert("Login Failed");
   }
 });
+export { loggedin };
