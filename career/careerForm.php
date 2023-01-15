ee3620e8d59340c11a5ec88821b2d42c3b5b2990
Hideki Natsumi <96385473+HidekiNatsumi@users.noreply.github.com>
2023-01-15 16:41:15 +0100

    change_inserting_forminfos_toDB

--- career/careerForm.php
+++ career/careerForm.php
@@ -36,7 +36,7 @@
       </nav>
     </header>
     <div class="login1">
-      <form class="form11">
+      <form class="form11" method="post">
         <h1 class="formstext">Apply</h1>
         <hr />
         <div class="name-div">
@@ -44,7 +44,7 @@
             <label class="name-l"></label>
           </div>
           <input
-            class="name"
+            class="username"
             type="text"
             name="name"
             placeholder="Name"
@@ -58,7 +58,7 @@
           <input
             class="lastname lastname1"
             type="text"
-            name="lastname"
+            name="empLastName"
             placeholder="Last Name"
             required
           />
@@ -70,7 +70,7 @@
           <input
             class="lastname jobposition"
             type="text"
-            name="lastname"
+            name="jobPosition"
             placeholder="Job Position"
             required
           />
@@ -82,23 +82,12 @@
           <input
             class="lastname phone"
             type="number"
-            name="lastname"
+            name="phone"
             placeholder="Phone Number"
             required
           />
         </div>
-        <div class="lastname-div">
-          <div>
-            <label class="lastname-l"></label>
-          </div>
-          <input
-            class="lastname degree"
-            type="text"
-            name="lastname"
-            placeholder="Degree"
-            required
-          />
-        </div>
+    
         <div class="lastname-div">
           <div>
             <label class="email-l"></label>
@@ -115,18 +104,12 @@
           <div>
             <a></a>
           </div>
-          <div class="zac">
-            <label for="zac">
-              <input type="file" name="file" class="file1 zac" />
-            </label>
-          </div>
         </div>
         <div <div class="sub">
           <input class="submit_btn" type="submit" value="Submit" />
         </div>
       </form>
     </div>
-    <script src="careerform.js"></script>
     <footer>
       <div class="foot">
    <div class="first1">
@@ -138,5 +121,41 @@
           <a class="copyR">Copyright © 2022 JobManagement. All Rights Reserved.</a>
       </div>
     </footer>
+    <?php
+    include "../database/database_con.php";
+    error_reporting(0);
+    include_once '../database/database_con.php';
+    //echo "Connected successfully";
+    $sql = 'SELECT id_user FROM accounts.applications;';
+     
+    //make query & get results
+      $result = mysqli_query($conn, $sql);
+
+      //fetch the resulting rows as an array
+      $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+  //free result from memory
+      mysqli_free_result($result);
+      $id=count($users)+1;
+      
+    
+    if (isset($_POST['name'])) {
+      $name = $_POST['name'];
+      $empLastName = $_POST['empLastName'];
+      $jobPosition = $_POST['jobPosition'];
+      $phone = $_POST['phone'];
+      $email = $_POST['email'];
+      $sql = "INSERT INTO `accounts`.`applications` (`id_user`, `username`, `empLastName`, `jobPosition`, `phone`, `email`) VALUES ('$id','$name', '$empLastName', '$jobPosition', '$phone', '$email')";
+      $result = mysqli_query($conn, $sql);
+      if ($result) {
+        echo "<script>alert('Your application has been submitted successfully!')</script>";
+      } else {
+        echo "<script>alert('Your application has not been submitted successfully!')</script>";
+      }
+    }
+    ?>
+    <script src="careerform.js"></script>
+
+  
   </body>
 </html>
