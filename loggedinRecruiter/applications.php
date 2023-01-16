bf4cede20e4ee5c1da314dad0d1b25cdbc8c0b9c
Hideki Natsumi <96385473+HidekiNatsumi@users.noreply.github.com>
2023-01-16 18:57:48 +0100

    change_info_intake

--- loggedinRecruiter/applications.php
+++ loggedinRecruiter/applications.php
@@ -55,7 +55,7 @@
 
             //fetch the resulting rows as an array
             $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
+            echo '<script>localStorage.setItem("usersLen",'.count($users).');</script>;';
         //free result from memory
             mysqli_free_result($result);
 
@@ -92,7 +92,10 @@
                   echo "<td class='row$count'>" . $users[$i]['phone'] . "</td>";
                   echo "<form method=`'get'`>";
                   $temp1=$users[$i]['username'];
-                  echo "<td class='row$count column1'><button name=`approve` class='searchbtn1 btn1' value='$temp1'>Approve</button><button name=`deny$count` class='searchbtn1' value='$temp1'>remove</button></td>";
+                  echo "<td class='row$count column1'>
+                  <button name=`approve$count` class='searchbtn2$count searchbtn' value='$temp1'>Approve</button>
+                  <button name=`deny$count` class='searchbtn1$count searchbtn1'  value='$temp1'>remove</button>
+                  </td>";
                   echo "</form>";
                   echo "</tr>";
            
