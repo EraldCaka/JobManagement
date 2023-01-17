dad9dbba49a4c5eafcf7b73bdcde4887da36f919
Hideki Natsumi <96385473+HidekiNatsumi@users.noreply.github.com>
2023-01-17 19:59:04 +0100

    change_remove_temp_items_logout

--- loggedin/logout.js
+++ loggedin/logout.js
@@ -2,8 +2,8 @@ console.log("logout.js");
 document.querySelector(".logout").addEventListener("click", function () {
   console.log("logout");
   //localStorage.setItem("loggedin", false);
-  localStorage.setItem("username");
-  localStorage.setItem("password");
+  localStorage.removeItem("username");
+  localStorage.removeItem("password");
 
   change();
 });
